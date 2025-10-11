eb4062dee059d098de5d9527d6ba6ff8d4e14087
Ragul-Devops <ragul493@gmail.com>
2025-10-11 15:45:22 +0530

    ragul.sh file added

--- ragul.sh
+++ ragul.sh
@@ -2,9 +2,9 @@
 
 echo "Starting script..."
 
-if [ $1 -gt 10  
+if [ $1 -gt 10 ]
 then
-    echo "Number is greater than 10"
+    echo "Number is greater than 10
 else
     echo "Number is 10 or less"
 fi
